511967bd7c8dd2bf3ae4b34e2ad1c89a84c79ee0
dpotoskuev <dpotoskuev@gmail.com>
2019-05-29 22:54:40 +0100

    constructor arguments instead of Parameters

--- src/main/kotlin/com/mma/contestmanager/view/intervals/IntervalFragment.kt
+++ src/main/kotlin/com/mma/contestmanager/view/intervals/IntervalFragment.kt
@@ -15,42 +15,43 @@ class IntervalFragment : Fragment() {
     }
 
     override val root = vbox {
-        headText?.let {label(headText!!)}
+        headText?.let { label(headText!!) }
         vbox {
             add(thumbsVBox)
             button("Add") {
                 action {
-                    val maxVal =intervalList.last().get()+10
+                    val maxVal = intervalList.last().get() + 10
                     intervalList.add(SimpleIntegerProperty(maxVal))
                 }
             }
         }
         handleChanges()
     }
+
     private fun handleChanges() {
         thumbsVBox.children.clear()
         // Add the first value
-        thumbsVBox.add(IntervalThumbFragment::class,
-                mapOf(
-                        IntervalThumbFragment::valueProperty to intervalList[0],
-                        IntervalThumbFragment::maxValProperty to intervalList[1],
-                        IntervalThumbFragment::minValProperty to SimpleIntegerProperty(0),
-                        IntervalThumbFragment::deleteAction to null))
+        thumbsVBox.add(IntervalThumbFragment(
+                SimpleIntegerProperty(0),
+                intervalList[1],
+                intervalList[0],
+                null))
+
         // Add all other values
         for (i in 1..intervalList.size - 2) {
-            thumbsVBox.add(IntervalThumbFragment::class,
-                    mapOf(
-                            IntervalThumbFragment::valueProperty to intervalList[i],
-                            IntervalThumbFragment::maxValProperty to intervalList[i + 1],
-                            IntervalThumbFragment::minValProperty to intervalList[i - 1],
-                            IntervalThumbFragment::deleteAction to {intervalList.remove(intervalList[i])}))
+            thumbsVBox.add(IntervalThumbFragment(
+                    intervalList[i - 1],
+                    intervalList[i + 1],
+                    intervalList[i])
+                    { intervalList.remove(intervalList[i]) })
+
         }
         // Add the last value
-        thumbsVBox.add(IntervalThumbFragment::class,
-                mapOf(
-                        IntervalThumbFragment::valueProperty to intervalList.last(),
-                        IntervalThumbFragment::minValProperty to intervalList.reversed().drop(1).first(),
-                        IntervalThumbFragment::maxValProperty to SimpleIntegerProperty(1000),
-                        IntervalThumbFragment::deleteAction to null))
+        thumbsVBox.add(IntervalThumbFragment(
+                intervalList.reversed().drop(1).first(),
+                SimpleIntegerProperty(1000),
+                intervalList.last(),
+                null))
+
     }
 }
--- src/main/kotlin/com/mma/contestmanager/view/intervals/IntervalThumbFragment.kt
+++ src/main/kotlin/com/mma/contestmanager/view/intervals/IntervalThumbFragment.kt
@@ -3,25 +3,27 @@ package com.mma.contestmanager.view.intervals
 import javafx.beans.property.SimpleIntegerProperty
 import tornadofx.*
 
-class IntervalThumbFragment : Fragment() {
-    val minValProperty: SimpleIntegerProperty by param()
-    val maxValProperty: SimpleIntegerProperty by param()
-    val valueProperty: SimpleIntegerProperty by param()
-    val deleteAction: (()->Unit)? by param()
+class IntervalThumbFragment(private val minValProperty: SimpleIntegerProperty,
+                            private val maxValProperty: SimpleIntegerProperty,
+                            private val valueProperty: SimpleIntegerProperty,
+                            private val deleteAction: (() -> Unit)?) : Fragment() {
+
     var value by valueProperty
 
     override val root = hbox {
         button("-") {
             enableWhen(valueProperty.add(-1).greaterThan(minValProperty))
-            action {value--}
+            action { value-- }
         }
-        label {textProperty().bind(valueProperty.asString())}
+        label { textProperty().bind(valueProperty.asString()) }
         button("+") {
             enableWhen(valueProperty.add(1).lessThan(maxValProperty))
             action { value++ }
         }
-        deleteAction?.let{button("Del") {
-            action {deleteAction!!()}
-        }}
+        deleteAction?.let {
+            button("Del") {
+                action { deleteAction.invoke() }
+            }
+        }
     }
 }
