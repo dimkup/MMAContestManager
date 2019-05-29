1199fd8622efa6bfbeae94d3d0616788391b791b
dpotoskuev <dpotoskuev@gmail.com>
2019-05-29 22:11:14 +0100

    Two interval fragments

--- src/main/kotlin/com/mma/contestmanager/view/CategoryTabView.kt
+++ src/main/kotlin/com/mma/contestmanager/view/CategoryTabView.kt
@@ -2,20 +2,27 @@ package com.mma.contestmanager.view
 
 import com.mma.contestmanager.view.intervals.IntervalFragment
 import javafx.beans.property.SimpleIntegerProperty
-import javafx.scene.layout.VBox
+import javafx.scene.layout.HBox
 import tornadofx.*
 
 class CategoryTabView : View("My View") {
-    override val root = VBox()
-    private val intList = mutableListOf(
+    override val root = HBox()
+    private val intList1 = mutableListOf(
+            SimpleIntegerProperty(5),
+            SimpleIntegerProperty(15),
+            SimpleIntegerProperty(20)).observable()
+    private val intList2 = mutableListOf(
             SimpleIntegerProperty(5),
             SimpleIntegerProperty(15),
             SimpleIntegerProperty(20)).observable()
     init {
         with(root) {
             add(IntervalFragment::class,mapOf(
-                    IntervalFragment::intervalList to intList,
+                    IntervalFragment::intervalList to intList1,
                     IntervalFragment::headText to "Weight"))
+            add(IntervalFragment::class,mapOf(
+                    IntervalFragment::intervalList to intList2,
+                    IntervalFragment::headText to "Age"))
         }
     }
 }
