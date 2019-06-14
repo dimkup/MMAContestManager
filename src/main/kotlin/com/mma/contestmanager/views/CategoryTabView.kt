7536e6c5d79992ee9b8bad766e4f7c25bed931ba
dpotoskuev <dpotoskuev@gmail.com>
2019-06-14 22:53:40 +0100

    Translations

--- src/main/kotlin/com/mma/contestmanager/views/CategoryTabView.kt
+++ src/main/kotlin/com/mma/contestmanager/views/CategoryTabView.kt
@@ -21,8 +21,8 @@ class CategoryTabView : View("My View") {
 
     init {
         with(root) {
-            add(IntervalFragment(intList1,"Weight", MIN_WHEIGT, MAX_WHEIGT, MAX_THUMBS))
-            add(IntervalFragment(intList2,"Age", MIN_AGE, MAX_AGE, MAX_THUMBS))
+            add(IntervalFragment(intList1,messages["weightIntervalsHead"], MIN_WHEIGT, MAX_WHEIGT, MAX_THUMBS))
+            add(IntervalFragment(intList2,messages["ageIntervalsHead"], MIN_AGE, MAX_AGE, MAX_THUMBS))
             spacing = 20.0
         }
     }
