6ab4d86b7a4bcd06a6ce6441ad26c4cc4e662984
dpotoskuev <dpotoskuev@gmail.com>
2019-07-10 17:56:26 +0100

    controller has been added to the categoryTab

--- src/main/kotlin/com/mma/contestmanager/controllers/RegistrationController.kt
+++ src/main/kotlin/com/mma/contestmanager/controllers/RegistrationController.kt
@@ -1,10 +1,24 @@
 package com.mma.contestmanager.controllers
 
+import com.mma.contestmanager.app.MIN_AGE
+import com.mma.contestmanager.app.MIN_WEIGHT
 import com.mma.contestmanager.models.Sportsman
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.collections.ObservableList
 import tornadofx.*
 
 class RegistrationController: Controller() {
     val sportsmen: ObservableList<Sportsman> = ArrayList<Sportsman>().observable()
+
+    val ageIntervals = mutableListOf(
+        SimpleIntegerProperty(MIN_AGE + 1),
+        SimpleIntegerProperty(15),
+        SimpleIntegerProperty(20)).observable()
+
+    val weightIntervals = mutableListOf(
+        SimpleIntegerProperty(MIN_WEIGHT + 1),
+        SimpleIntegerProperty(15),
+        SimpleIntegerProperty(20)).observable()
+
 
 }
--- src/main/kotlin/com/mma/contestmanager/views/CategoryTabView.kt
+++ src/main/kotlin/com/mma/contestmanager/views/CategoryTabView.kt
@@ -1,28 +1,20 @@
 package com.mma.contestmanager.views
 
 import com.mma.contestmanager.app.*
+import com.mma.contestmanager.controllers.RegistrationController
 import com.mma.contestmanager.views.intervals.IntervalFragment
-import javafx.beans.property.SimpleIntegerProperty
 import javafx.scene.layout.HBox
 import tornadofx.*
 
 class CategoryTabView : View() {
+    private val controller: RegistrationController by inject()
     override val root = HBox()
-    private val intList1 = mutableListOf(
-        SimpleIntegerProperty(MIN_WEIGHT + 1),
-        SimpleIntegerProperty(15),
-        SimpleIntegerProperty(20)
-    ).observable()
-    private val intList2 = mutableListOf(
-        SimpleIntegerProperty(MIN_AGE + 1),
-        SimpleIntegerProperty(15),
-        SimpleIntegerProperty(20)
-    ).observable()
+
 
     init {
         with(root) {
-            add(IntervalFragment(intList1,messages["weight"], MIN_WEIGHT, MAX_WEIGHT, MAX_THUMBS))
-            add(IntervalFragment(intList2,messages["age"], MIN_AGE, MAX_AGE, MAX_THUMBS))
+            add(IntervalFragment(controller.weightIntervals,messages["weight"], MIN_WEIGHT, MAX_WEIGHT, MAX_THUMBS))
+            add(IntervalFragment(controller.ageIntervals,messages["age"], MIN_AGE, MAX_AGE, MAX_THUMBS))
             spacing = 20.0
         }
     }
